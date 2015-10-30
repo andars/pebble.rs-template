43fe5994c10563b9db11de23cde672c2945969e6
andars <afoote97@gmail.com>
2015-10-30 00:30:28 -0600

    add shameful comment to build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ set -k
 
 cargo build -p pebble --target arm-none-eabi --release
 
+#this is pretty hacky but seems to work
+#  i'm sure there is a better way
+#  but this allows me to still use pebble's waf system
 cd target/arm-none-eabi/release/deps
 ar xv *.a
 cd -
